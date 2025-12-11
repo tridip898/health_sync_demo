5edf880db156b5f5bf82fcb992755288d5d457d9
“saif” <saiful.islam@technonext.com>
2025-12-11 12:49:11 +0600

    registration page redesigned done

--- lib/app/modules/registration/views/registration_view.dart
+++ lib/app/modules/registration/views/registration_view.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:health_sync_question/app/core/extensions/widget_extension.dart';
 import 'package:health_sync_question/app/core/utils/app_input_validator.dart';
 import 'package:health_sync_question/app/core/widgets/custom_app_bar.dart';
@@ -15,77 +16,55 @@ class RegistrationView extends GetView<RegistrationController> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: CustomAppBar(title: 'Registration'),
-      body: Column(
-        children: [
-          const SizedBox(height: 40),
-
-          // Top AppBar
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 16),
-            child: Row(
-              children: [
-                Container(
-                  height: 48,
-                  width: 48,
-                  alignment: Alignment.center,
-                  child: const Icon(Icons.arrow_back, size: 26),
-                ),
-                Expanded(
-                  child: Text(
-                    "Create your account",
-                    textAlign: TextAlign.center,
-                    style: textStyle.bold.s36,
-                  ),
-                ),
-                const SizedBox(width: 48),
-              ],
+      appBar: CustomAppBar(title: 'Create your account'),
+      body: Center(
+        child: Column(
+          children: [
+            const SizedBox(height: 40),
+            // Body Text
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16),
+              child: Text(
+                "Please enter your phone number. We use it to verify your identity and secure your medical information.",
+                style: textStyle.regular.s14.copyWith(color: Colors.black),
+              ),
             ),
-          ),
 
-          const SizedBox(height: 20),
+            const SizedBox(height: 20),
 
-          // Body Text
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 16),
-            child: Text(
-              "Please enter your phone number. We use it to verify your identity and secure your medical information.",
-              style: textStyle.medium.s18,
+            Padding(
+              padding: EdgeInsets.all(12),
+              child: CustomTextFormField(
+                labelText: 'Phone Number',
+                hintText: '01*********',
+                controller: controller.phoneController,
+                keyboardType: TextInputType.phone,
+                validator: AppInputValidator.bdPhoneValidator,
+                autoValidateMode: AutovalidateMode.onUserInteraction,
+              ),
             ),
-          ),
 
-          const SizedBox(height: 20),
-
-          // Phone Input Field
-          // Phone
-          CustomTextFormField(
-            labelText: 'Phone Number',
-            hintText: '01*********',
-            controller: controller.phoneController,
-            keyboardType: TextInputType.phone,
-            validator: AppInputValidator.bdPhoneValidator,
-            autoValidateMode: AutovalidateMode.onUserInteraction,
-          ),
-
-          const Spacer(),
-
-          // Terms
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 16),
-            child: Text(
-              "By continuing, you agree to our Terms of Service and Privacy Policy.",
-              textAlign: TextAlign.center,
-              style: textStyle.regular.s16,
+            const Spacer(),
+            // Terms
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16),
+              child: Text(
+                "By continuing, you agree to our Terms of Service and Privacy Policy.",
+                style: textStyle.regular.s14.copyWith(color:const Color(0xFF4C9A80)),
+              ),
             ),
-          ),
 
-          const SizedBox(height: 20),
+            const SizedBox(height: 20),
 
-          // Continue Button
-          CustomButton(text: 'Send Otp', onPressed: controller.onSendOtp),
+            // Continue Button
+            Padding(
+              padding: const EdgeInsets.all(12.0),
+              child: CustomButton(text: 'Send Otp', onPressed: controller.onSendOtp),
+            ),
 
-          const SizedBox(height: 30),
-        ],
+            const SizedBox(height: 30),
+          ],
+        ),
       ),
     );
   }
