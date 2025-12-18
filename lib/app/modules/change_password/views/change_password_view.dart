9c1e1534c1bb569ff7f5df4f847b10fe363dbff4
“saif” <saiful.islam@technonext.com>
2025-12-18 17:11:47 +0600

    view file added for forgot password module

---
+++ lib/app/modules/change_password/views/change_password_view.dart
@@ -0,0 +1,72 @@
+import 'package:flutter/material.dart';
+
+import 'package:get/get.dart';
+
+import '../../../core/extensions/widget_extension.dart';
+import '../../../core/widgets/custom_button.dart';
+import '../../../core/widgets/custom_text_field.dart';
+import '../controllers/change_password_controller.dart';
+
+class ChangePasswordView extends GetView<ChangePasswordController> {
+  const ChangePasswordView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Reset Password'),
+        centerTitle: true,
+      ),
+      body: SafeArea(
+        child: Form(
+          key: controller.formKey,
+          child: SingleChildScrollView(
+            padding: const EdgeInsets.all(14),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  'Create a strong password to protect your medical history.',
+                  style: textStyle.bold.s14.copyWith(color: Colors.grey),
+                ),
+                const SizedBox(height: 20),
+
+                CustomTextFormField(
+                  controller: controller.passwordController,
+                  labelText: 'New Password',
+                  validator: (value) {
+                    if (value == null || value.length < 8) {
+                      return "Password must be at least 8 characters";
+                    }
+                    return null;
+                  }, hintText: '...',
+                ),
+
+                const SizedBox(height: 12),
+
+                CustomTextFormField(
+                  controller: controller.confirmPasswordController,
+                  labelText: 'Confirm Password',
+
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return "Confirm password required";
+                    }
+                    return null;
+                  }, hintText: '',
+                ),
+
+                const SizedBox(height: 30),
+
+                CustomButton(
+                  text: 'Reset Password',
+                  onPressed: controller.onResetPassword,
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
