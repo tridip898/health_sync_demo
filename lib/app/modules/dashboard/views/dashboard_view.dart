042e5f60e87db716d60bc892559b88357a2d1ad8
abusayed10809 <abusayed10809@gmail.com>
2025-12-22 16:20:42 +0600

    updated dashboard controller

--- lib/app/modules/dashboard/views/dashboard_view.dart
+++ lib/app/modules/dashboard/views/dashboard_view.dart
@@ -133,7 +133,10 @@ class _Header extends StatelessWidget {
               ),
             ],
           ),
-          IconButton(icon: const Icon(Icons.settings), onPressed: () {}),
+          IconButton(
+            icon: const Icon(Icons.logout),
+            onPressed: appController.signOut,
+          ),
         ],
       ),
     );
